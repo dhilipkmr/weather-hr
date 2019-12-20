85729110d1ace00d31a90823408c9422ac94c555
dhilipkumarV <dhilip.velusamy@go-mmt.com>
2019-12-20 20:38:33 +0530

    merge

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'view')));
 app.use(express.static(__dirname));
-app.listen(8000, '0.0.0.0', function() {});
+app.listen(8000, '0.0.0.0', function() { console.log('Application is running on http://localhost:8000')});
 module.exports = app;
